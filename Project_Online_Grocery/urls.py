a89f5acffda6e58c9b8299d1542ef3f7bd13612d
rashika-machhi <machhirashika@gmail.com>
2023-11-22 22:48:45 +0530

    project Settings and Create apps

--- Project_Online_Grocery/urls.py
+++ Project_Online_Grocery/urls.py
@@ -1,22 +1,73 @@
 """
-URL configuration for Project_Online_Grocery project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+*************************************
+        Imported Packages 
+*************************************
 """
+
+# By Default
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# Static File
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Token For Login
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+"""
+*************************************
+    Swagger Setting For ULRS
+*************************************
+"""
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Online_Grocery_Shop",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, ),
+)
+
+"""
+**************************************************************************
+                            ULRS
+**************************************************************************
+"""
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+    # Default URL of JWT
+    # path("loginjwt/", TokenObtainPairView.as_view(), name="loginjwt"),
+    path("refresh/", TokenRefreshView.as_view(), name="refreshtoken"),
+
+    # Swagger
+    path('swaggerr(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+                                       cache_timeout=0), name='schema-redoc'),
+
+    # Django Admin Panel
+    path('adminPanel/', admin.site.urls),
+
+    # App Admin Portal
+    path("admin/", include("app_admin.urls")),
+
+    # App Shop Portal
+    path("", include("app_shop.urls")),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
